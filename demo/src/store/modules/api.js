d66c4fe0ca738f2ead85b10de0996e76e984c6fe
Brian Schiller <bgschiller@gmail.com>
2018-04-25 10:23:43 -0600

    update cors proxy

--- demo/src/store/modules/api.js
+++ demo/src/store/modules/api.js
@@ -4,13 +4,13 @@ export const getCats = async (payload) => {
   if (!payload) return null;
   const { numResults, category, size } = payload;
   const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/thecatapi.com/api/images/get?format=xml&results_per_page=${numResults}&category=${category}&size=${size}`);
+    `https://cors-nowhere.glitch.me/https://thecatapi.com/api/images/get?format=xml&results_per_page=${numResults}&category=${category}&size=${size}&api_key=${catApiKey}`);
   const text = await response.text();
   return text;
 };
 
 export const getCategories = async () => {
-  const response = await fetch('https://cors-anywhere.herokuapp.com/thecatapi.com/api/categories/list');
+  const response = await fetch(`https://cors-nowhere.glitch.me/https://thecatapi.com/api/categories/list?api_key=${catApiKey}`);
   const text = await response.text();
   return text;
 };
